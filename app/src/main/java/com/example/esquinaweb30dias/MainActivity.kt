8b1c73afc3c2f54c8cf888a23eec15aa0b60081c
Jhon Mantilla <kaiser_4tu@hotmail.com>
2024-06-12 18:20:27 -0500

    Terminar app primera parte titulo, imagen, boton y contenido

--- app/src/main/java/com/example/esquinaweb30dias/MainActivity.kt
+++ app/src/main/java/com/example/esquinaweb30dias/MainActivity.kt
@@ -1,16 +1,49 @@
 package com.example.esquinaweb30dias
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.esquinaweb30dias.data.Entradas
+import com.example.esquinaweb30dias.data.entradas
 import com.example.esquinaweb30dias.ui.theme.EsquinaWeb30DíasTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +52,176 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EsquinaWeb30DíasTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                    EntradasApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun EntradasApp() {
+
+    Scaffold(
+        topBar = {
+            EntradaTopAppBar()
+        }
+    ) { it->
+        LazyColumn(contentPadding = it) {
+            items(entradas){
+                EntradasItem(
+                    entrada = it
+                )
+            }
+        }
+
+    }
+    //EntradasItem(entrada = Entradas(1,  R.string.title_1, R.string.content_1, R.drawable.ydray))
+}
+@Composable
+fun EntradasItem(
+    entrada: Entradas,
+    modifier: Modifier = Modifier
+){
+    var expanded by remember { mutableStateOf(false) }
+    val color by animateColorAsState(
+        targetValue = if (expanded) MaterialTheme.colorScheme.tertiaryContainer
+        else MaterialTheme.colorScheme.primaryContainer
+    )
+
+    Card(
+        modifier = Modifier
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            ).padding(10.dp)
+
+    ){
+        Column (modifier = Modifier.background(color = color)) {
+            EntradaInfo(
+                entradaTitle = entrada.title,
+                entradaId = entrada.id,
+                modifier = Modifier.padding(10.dp))
+            EntradaImagen(
+                entradaImage = entrada.image,
+                modifier = Modifier
+                    .padding(
+                        start = 10.dp,
+                        end = 10.dp
+                    )
+                    .clip(MaterialTheme.shapes.small)
+            )
+            EntradaItemButtom(
+                expanded = expanded,
+                onClick = { expanded = !expanded },
+                modifier = Modifier
+            )
+            if (expanded){
+                EntradaContent(
+                    entradaContent = entrada.content,
+                    modifier = Modifier.padding(
+                        start = 10.dp,
+                        end = 10.dp,
+                        bottom = 10.dp,
+                    )
+                )
+            }
+
+        }
+    }
+}
+
+@Composable
+fun EntradaInfo(
+    @StringRes entradaTitle: Int,
+    @StringRes entradaId: Int,
+    modifier: Modifier = Modifier
+){
+    Column (modifier = modifier) {
+        Text(
+            text = "Día: " + entradaId.toString(),
+            style = MaterialTheme.typography.labelSmall
+            )
+        Text(
+            text = stringResource(id = entradaTitle),
+            style = MaterialTheme.typography.titleLarge
+        )
+    }
+}
+@Composable
+fun EntradaImagen(
+    @DrawableRes entradaImage: Int,
+    modifier: Modifier = Modifier
+){
+    Image(
+        painter = painterResource(id = entradaImage),
+        contentDescription = null,
         modifier = modifier
     )
 }
 
+@Composable
+fun EntradaItemButtom(
+    expanded: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    IconButton(
+        onClick = onClick,
+        modifier = modifier
+    ){
+        Icon(
+            imageVector = Icons.Filled.ExpandMore,
+            contentDescription = stringResource(R.string.expand_button_content_description),
+            tint = MaterialTheme.colorScheme.secondary
+        )
+    }
+}
+
+@Composable
+fun EntradaContent(
+    @StringRes entradaContent: Int,
+    modifier: Modifier = Modifier
+){
+    Column(modifier = modifier) {
+        Text(
+            text = stringResource(id = entradaContent),
+            style = MaterialTheme.typography.labelSmall
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntradaTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Row (verticalAlignment = Alignment.CenterVertically){
+                Image(
+                    modifier = Modifier
+                        .size(64.dp)
+                        .padding(8.dp),
+                    painter = painterResource(R.drawable.logo_esquina_web),
+                    contentDescription = null
+                )
+                Text(text = stringResource(R.string.app_name), style = MaterialTheme.typography.bodyLarge)
+            }
+        }
+    )
+}
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    EsquinaWeb30DíasTheme {
-        Greeting("Android")
+fun EntradasPreviewDark() {
+    EsquinaWeb30DíasTheme(darkTheme = true) {
+        EntradasApp()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun EntradasPreview() {
+    EsquinaWeb30DíasTheme(darkTheme = false) {
+        EntradasApp()
     }
 }
--- app/src/main/java/com/example/esquinaweb30dias/data/Entradas.kt
+++ app/src/main/java/com/example/esquinaweb30dias/data/Entradas.kt
@@ -12,8 +12,8 @@ data class Entradas(
 )
 
 val entradas = listOf(
-    Entradas(1, R.string.title_1, R.string.content_1, R.drawable.YDRAY),
-    Entradas(2, R.string.title_2, R.string.content_2, R.drawable.PngEgg),
+    Entradas(1, R.string.title_1, R.string.content_1, R.drawable.ydray),
+    Entradas(2, R.string.title_2, R.string.content_2, R.drawable.pngegg),
     Entradas(3, R.string.title_3, R.string.content_3, R.drawable.delta_emulator),
     Entradas(4, R.string.title_4, R.string.content_4, R.drawable.tinywow),
     Entradas(5, R.string.title_5, R.string.content_5, R.drawable.vanitas),
@@ -21,10 +21,10 @@ val entradas = listOf(
     Entradas(7, R.string.title_7, R.string.content_7, R.drawable.conuter_strike_2),
     Entradas(8, R.string.title_8, R.string.content_8, R.drawable.eliminardevoz),
     Entradas(9, R.string.title_9, R.string.content_9, R.drawable.itch_io),
-    Entradas(10, R.string.title_10, R.string.content_10, R.drawable.Powtoon),
+    Entradas(10, R.string.title_10, R.string.content_10, R.drawable.powtoon),
     Entradas(11, R.string.title_11, R.string.content_11, R.drawable.svgrepo),
     Entradas(12, R.string.title_12, R.string.content_12, R.drawable.rec_cloud),
     Entradas(13, R.string.title_13, R.string.content_13, R.drawable.hells_paradise),
-    Entradas(14, R.string.title_14, R.string.content_14, R.drawable.Drawio),
-    Entradas(15, R.string.title_15, R.string.content_15, R.drawable.CamScanner)
+    Entradas(14, R.string.title_14, R.string.content_14, R.drawable.drawio),
+    Entradas(15, R.string.title_15, R.string.content_15, R.drawable.cam_scanner)
 )
