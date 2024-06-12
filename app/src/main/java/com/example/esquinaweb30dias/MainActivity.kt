73b2c92008d1e08ae10f914625ceeec15fecf2c5
Jhon Mantilla <kaiser_4tu@hotmail.com>
2024-06-12 12:37:15 -0500

    inicio proyceto 30 días de notas

---
+++ app/src/main/java/com/example/esquinaweb30dias/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.esquinaweb30dias
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.esquinaweb30dias.ui.theme.EsquinaWeb30DíasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            EsquinaWeb30DíasTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    EsquinaWeb30DíasTheme {
+        Greeting("Android")
+    }
+}
